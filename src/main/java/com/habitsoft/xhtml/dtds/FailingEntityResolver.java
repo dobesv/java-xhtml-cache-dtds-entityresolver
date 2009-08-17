a84eb103a3126c6d1a779e57f7124be92fb0aa4e
yonas.jongkind <yonas.jongkind@834c98fe-da1b-11dd-86c2-0f8696b7b6f9>
2009-08-17 18:26:37 +0000

    Was using junit.

--- src/main/java/com/habitsoft/xhtml/dtds/FailingEntityResolver.java
+++ src/main/java/com/habitsoft/xhtml/dtds/FailingEntityResolver.java
@@ -2,7 +2,6 @@ package com.habitsoft.xhtml.dtds;
 
 import java.io.IOException;
 
-import org.junit.Assert;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -11,8 +10,7 @@ public class FailingEntityResolver implements EntityResolver {
 
     @Override
     public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-        Assert.fail("Entity should have been resolved from the cache: "+systemId);
-        return null;
+        throw new IllegalStateException("Entity should have been resolved from the cache: "+systemId);
     }
 
 }
